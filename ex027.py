b229b3324113be84469396ebe7851341fdf6a705
Viteit <vitorcarvalhop48@gmail.com>
2022-11-14 12:40:22 -0300

    making change in the file

--- ex027.py
+++ ex027.py
@@ -1,2 +1,5 @@
-frase = 'Curso em Video Python'
-print()
+n = str(input('Digite seu nome compelto: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
